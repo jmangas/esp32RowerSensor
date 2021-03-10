01e7fb49642f9cc002d49d7a0d9654446d1a02b8
jmangas <jmangas@uoc.edu>
2021-03-10 23:23:32 +0100

    SerLCD + RPC

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
-# UART JavaScript API usage example
+# esp32 Rower sensor
 
-See comments in the `fs/init.js` source file.
+## 
+
+    sudo chmod a+rw /dev/ttyUSB0
+
+
+## RPC
+
+    curl http://192.168.1.130/rpc/Led -d '{"a":1, "b": 2}'
+
+https://mongoose-os.com/docs/mongoose-os/userguide/rpc.md
+
+
+## SerLCD
+https://docs.google.com/document/d/1Jg1R2eXgXl7fwOFfjCIDnrc-6mAETJV2R8NGf-ptP0Q/edit
+
+Control Character
+Command
+Description
+0xFE
+0x01
+Clear Display
+0x14
+Move Cursor Right 1 Space
+0x10
+Move Cursor Left 1 Space
+0x1C
+Scroll Right 1 Space
+0x18
+Scroll Left 1 Space
+0x0C
+Turn Display On / Hide Cursor
+0x08
+Turn Display Off
+0x0E
+Underline Cursor On
+0x0D
+Blinking Cursor On
+0x80 + n
+Set Cursor Position
+0x7C
+0x03
+20 Characters Wide
+0x04
+16 Characters Wide
+0x05
+4 Lines
+0x06
+2 Lines
+0x0A
+Set Splash Screen
+0x0B
+Set Baud Rate to 2400
+0x0C
+Set Baud Rate to 4800
+0x0D
+Set Baud Rate to 9600
+0x0E
+Set Baud Rate to 14400
+0x0F
+Set Baud Rate to 19200
+0x10
+Set Baud Rate to 38400
+0x80
+Backlight Off*
+0x9D
+Backlight Fully On*
+0x12
+Reset to 9600 Baud - send during boot-up
+
 
--- fs/init.js
+++ fs/init.js
@@ -1,15 +1,37 @@
 load('api_timer.js');
 load('api_uart.js');
 load('api_sys.js');
-
+load('api_config.js');
+load('api_events.js');
+load('api_gpio.js');
+load('api_rpc.js');
+//load('api_mqtt.js');
+//load('api_shadow.js');
+//load('api_aws.js');
+//load('api_dash.js');
 
 let uartNo = 1;   // Uart number used for this example
 let rxAcc = '';   // Accumulated Rx data, will be echoed back to Tx
 let value = false;
+let btn = Cfg.get('board.btn1.pin');              // Built-in button GPIO
+let led = 2;              // Built-in LED GPIO number
+let onhi = Cfg.get('board.led1.active_high');     // LED on when high?
+let state = {on: false, btnCount: 0, uptime: 0};  // Device state
+let online = false;                               // Connected to the cloud?
 
-// Configure UART at 115200 baud
+let setLED = function(on) {
+  let level = onhi ? on : !on;
+  GPIO.write(led, level);
+  print('LED on ->', on);
+};
+
+GPIO.set_mode(led, GPIO.MODE_OUTPUT);
+setLED(state.on);
+
+
+// Configure UART at 9600 baud
 UART.setConfig(uartNo, {
-  baudRate: 115200,
+  baudRate: 9600,
   esp32: {
     gpio: {
       rx: 16,
@@ -34,17 +56,82 @@ UART.setDispatcher(uartNo, function(uartNo) {
 // Enable Rx
 UART.setRxEnabled(uartNo, true);
 
+UART.write( uartNo, "\xFE");
+UART.write( uartNo, "\x01"); // Clear display
+UART.write( uartNo, "\x14");
+UART.write( uartNo, "OK");
+
 // Send UART data every second
-Timer.set(1000 /* milliseconds */, Timer.REPEAT, function() {
-  value = !value;
-  UART.write(
-    uartNo,
-    'Hello UART! '
-      + (value ? 'Tick' : 'Tock')
-      + ' uptime: ' + JSON.stringify(Sys.uptime())
-      + ' RAM: ' + JSON.stringify(Sys.free_ram())
-      + (rxAcc.length > 0 ? (' Rx: ' + rxAcc) : '')
-      + '\n'
-  );
-  rxAcc = '';
+// Timer.set(1000 /* milliseconds */, Timer.REPEAT, function() {
+//   value = !value;
+//   UART.write(
+//     uartNo,
+//     'Hello UART! '
+//       + (value ? 'Tick' : 'Tock')
+//       + ' uptime: ' + JSON.stringify(Sys.uptime())
+//       + ' RAM: ' + JSON.stringify(Sys.free_ram())
+//       + (rxAcc.length > 0 ? (' Rx: ' + rxAcc) : '')
+//       + '\n'
+//   );
+//   rxAcc = '';
+// }, null);
+// Update state every second, and report to cloud if online
+Timer.set(6000, Timer.REPEAT, function() {
+  state.uptime = Sys.uptime();
+  state.ram_free = Sys.free_ram();
+  print('online:', online, JSON.stringify(state));
+  if (online) reportState();
 }, null);
+
+if (btn >= 0) {
+  let btnCount = 0;
+  let btnPull, btnEdge;
+  if (Cfg.get('board.btn1.pull_up') ? GPIO.PULL_UP : GPIO.PULL_DOWN) {
+    btnPull = GPIO.PULL_UP;
+    btnEdge = GPIO.INT_EDGE_NEG;
+  } else {
+    btnPull = GPIO.PULL_DOWN;
+    btnEdge = GPIO.INT_EDGE_POS;
+  }
+  
+  GPIO.set_button_handler(btn, btnPull, btnEdge, 20, function() {
+    state.btnCount++;
+    state.on = !state.on;
+    setLED(state.on);
+
+    let message = JSON.stringify(state);
+    print(message);
+    let sendMQTT = true;
+    // if (Dash.isConnected()) {
+    //   print('== Click!');
+    //   // TODO: Maybe do something else?
+    //   sendMQTT = false;
+    // }
+    // // AWS is handled as plain MQTT since it allows arbitrary topics.
+    // if (AWS.isConnected() || (MQTT.isConnected() && sendMQTT)) {
+    //   let topic = 'devices/' + Cfg.get('device.id') + '/events';
+    //   print('== Publishing to ' + topic + ':', message);
+    //   MQTT.pub(topic, message, 0 /* QoS */);
+    // } else if (sendMQTT) {
+    //   print('== Not connected!');
+    // }
+  }, null);
+}
+
+RPC.addHandler('Led', function(args) {
+  if (typeof(args) === 'object' ) {
+    state.on = !state.on;
+    setLED(state.on);
+    return state.on;
+  } else {
+    return {error: -1, message: 'Bad request. Expected: {"a":N1,"b":N2}'};
+  }
+});
+
+RPC.addHandler('Sum', function(args) {
+  if (typeof(args) === 'object' && typeof(args.a) === 'number' && typeof(args.b) === 'number') {
+    return args.a + args.b;
+  } else {
+    return {error: -1, message: 'Bad request. Expected: {"a":N1,"b":N2}'};
+  }
+});
